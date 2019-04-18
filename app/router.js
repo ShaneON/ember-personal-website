f28380952fefc7899b031a559902d64549a96854
Shane <shane@Shanes-MacBook-Air.local>
2019-04-18 17:28:23 +0100

    added content for home and experience routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('home', { path: '/'});
+  this.route('experience');
+  this.route('contact');
 });
 
 export default Router;
