c719c3b4eaafb4fd3243b73bd8a809d95b205237
Shane <sgoneill89@gmail.com>
2019-04-22 19:01:49 +0100

    added sticky header for scroll

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('header', "header")
+    this._checkScroll(controller);
+  },
+
+  _checkScroll(controller) {
+    window.addEventListener('scroll', (() => {
+      let scrollPosition = Math.round(window.scrollY);
+      if (scrollPosition > 100){
+        controller.set('header', "sticky");
+      }
+      else {
+        controller.set('header', "header");
+      }
+    }));
+  }
+
+
 });
